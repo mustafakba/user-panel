36da62fde21939839054e5e521b83b7e3f13af30
mustafakba <kaba.mustafaa@gmail.com>
2022-09-23 10:46:04 +0300

    sayfa yenilendiğinde token aktif kalması

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import axios from "axios";
+import { router } from "./router";
 
 Vue.use(Vuex);
 
@@ -17,6 +18,17 @@ const store = new Vuex.Store({
     },
   },
   actions: {
+    initAuth({commit}){
+      let token = localStorage.getItem("token");
+      if(token){
+        commit("setToken",token)
+        router.push("/home")
+      }
+      else{
+        router.push("/")
+        return false
+      }
+    },
     signUp({ commit },authData) {
         axios.post(
             "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAyTPu80Gn-QT3Phb7fomSc-d6sFkrn1mw",
@@ -43,11 +55,13 @@ const store = new Vuex.Store({
         )
         .then((response) => {
             commit("setToken",response.data.idToken)
-            console.log("kullanıcı girişi yapıldı")
-        });
+            localStorage.setItem("token",response.data.idToken)
+        
+          });
     },
     logout({ commit,}) {
       commit("clearToken")
+      localStorage.removeItem("token")
     },
   },
   getters: {
